3cf9857a7720ce211438e895366bb22c122e080f
Khushi-Bhatia <khushibhatiaabc@gmail.com>
2021-10-15 22:10:35 +0530

    added bubblesort file

---
+++ bubblesort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main()
+{
+    int n,i,j,a[100],temp;
+    printf("enter size of array:");
+    scanf("%d",&n);
+    printf("enter elements of array:");
+    for(i=0;i<n;i++)
+    {scanf("%d",&a[i]);}
+    
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+}
